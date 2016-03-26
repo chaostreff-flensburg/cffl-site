e16bb823bd8fb83901ef08ad5410831818755ca9
spralexx <mail@rene-krick.de>
2016-03-27 00:04:34 +0100

    prepare for 2016

--- site.mk
+++ site.mk
@@ -1,7 +1,7 @@
 GLUON_SITE_PACKAGES := \
 	gluon-mesh-batman-adv-15 \
 	gluon-alfred \
-	gluon-announced \
+	gluon-respondd \
 	gluon-autoupdater \
 	gluon-config-mode-autoupdater \
 	gluon-config-mode-contact-info \
@@ -27,8 +27,6 @@ GLUON_SITE_PACKAGES := \
 	kmod-usb-core \
 	kmod-usb2 \
 	kmod-usb-hid \
-	kmod-usb-net \
-	kmod-usb-net-asix \
 	kmod-r8169
 
 DEFAULT_GLUON_RELEASE := beta-2016.1.0-0
